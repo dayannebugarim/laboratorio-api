24b97cbb0d4847a22a0ce0b5f7671b534c4a150b
Dayanne Bugarim <dayanne.bugarim@iexfy.com>
2024-03-18 08:50:45 -0300

    fix: middleware timezone

--- src/controllers/labController.js
+++ src/controllers/labController.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const PDFDocument = require('pdfkit');
-const laboratorios = require('../data/labs.json');
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const laboratorios = require("../data/labs.json");
 
 exports.obterTodos = (req, res) => {
   res.json(laboratorios);
@@ -10,16 +10,16 @@ exports.cadastrarNovo = (req, res) => {
   const novoLaboratorio = req.body;
   laboratorios.push(novoLaboratorio);
 
-  fs.writeFileSync('src/data/labs.json', JSON.stringify(laboratorios, null, 2));
+  fs.writeFileSync("src/data/labs.json", JSON.stringify(laboratorios, null, 2));
 
-  res.status(201).json({ message: 'Laboratório cadastrado com sucesso!' });
+  res.status(201).json({ message: "Laboratório cadastrado com sucesso!" });
 };
 
 exports.gerarRelatorio = (req, res) => {
   const doc = new PDFDocument();
-  doc.pipe(fs.createWriteStream('relatorio-laboratorios.pdf'));
+  doc.pipe(fs.createWriteStream("relatorio-laboratorios.pdf"));
 
-  doc.fontSize(14).text('Relatório de Laboratórios\n\n');
+  doc.fontSize(14).text("Relatório de Laboratórios\n\n");
   laboratorios.forEach((lab, index) => {
     doc.fontSize(12).text(`Laboratório ${index + 1}: ${lab.nome}`);
     doc.fontSize(10).text(`Capacidade: ${lab.capacidade}`);
@@ -27,5 +27,5 @@ exports.gerarRelatorio = (req, res) => {
   });
 
   doc.end();
-  res.download('relatorio-laboratorios.pdf');
+  res.download("relatorio-laboratorios.pdf");
 };
--- src/middleware/accessMiddleware.js
+++ src/middleware/accessMiddleware.js
@@ -1,12 +1,16 @@
 module.exports = (req, res, next) => {
-  const now = new Date();
-  const hour = now.getHours();
-  console.log(now)
-  console.log(hour)
+  const timezone = "America/Sao_Paulo";
+  const now = new Date().toLocaleString("en-US", { timeZone: timezone });
+  const hour = new Date(now).getHours();
 
-  if (hour >= 8 && hour < 0) {
+  console.log(now);
+  console.log(hour);
+
+  if (hour >= 8 && hour < 17) {
     next();
   } else {
-    res.status(403).json({ message: 'Acesso permitido apenas entre 08:00 e 17:00.' });
+    res
+      .status(403)
+      .json({ message: "Acesso permitido apenas entre 08:00 e 17:00." });
   }
 };
