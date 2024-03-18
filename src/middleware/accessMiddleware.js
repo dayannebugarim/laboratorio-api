a5e9719f091a2b278489562f70ac501b07d808ee
Dayanne Bugarim <dayanne.bugarim@iexfy.com>
2024-03-17 23:00:25 -0300

    log hours

--- src/middleware/accessMiddleware.js
+++ src/middleware/accessMiddleware.js
@@ -1,8 +1,10 @@
 module.exports = (req, res, next) => {
   const now = new Date();
   const hour = now.getHours();
+  console.log(now)
+  console.log(hour)
 
-  if (hour >= 8 && hour < 23) {
+  if (hour >= 8 && hour < 0) {
     next();
   } else {
     res.status(403).json({ message: 'Acesso permitido apenas entre 08:00 e 17:00.' });
